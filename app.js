97f70d7b3928d21eaa594856cb57fa6540a0723c
Asahan Sahan <asahansahan@asahans-mbp-2.home>
2023-01-30 21:12:56 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require("path");
+
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+
+app.get('/product', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+  
+
+
+app.use('/contact', (req, res, next)=> {
+    res.send('<h1>hello from contact</h1>');
+});
+
+app.use('/', (req, res, next)=> {
+    res.send('<h1>hello from node</h1>');
+});
+
+
+
+app.listen(3000, (req, res, next) => {
+    console.log('server is started on port 3000');
+});
+
+
+
+
